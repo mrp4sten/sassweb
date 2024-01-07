46f49543958c046677c25c9b368f5507a6d8bccd
mrp4sten <mauricio.pasten.martinez@gmail.com>
2024-01-06 21:15:06 -0600

    Add navbar collapse functionality

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const navBarToggler = document.querySelector(".navbar__top--toggler");
+const navBarCollapse = document.querySelector(".navbar__collapse");
+
 navBarToggler.addEventListener("click", () => {
   navBarToggler.classList.toggle("change");
+  navBarCollapse.classList.toggle("show__nav")
 });
